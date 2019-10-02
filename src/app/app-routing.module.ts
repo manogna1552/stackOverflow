7916e1933ca33a19fde7b00a198519e019098db9
manogna <manogna.madineni6@gmail.com>
2019-10-02 14:27:33 -0400

    apps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () =>
+    import('./main-home/main-home.module').then(
+      m => m.MainHomeModule
+    )
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/main-home/main-home-routing.module.ts
+++ src/app/main-home/main-home-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
 
-
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      // {
+      //   path: 'portfolio',
+      //   loadChildren: () =>
+      //     import('../portfolio/portfolio.module').then(
+      //       m => m.PortfolioModule
+      //     )
+      // }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
